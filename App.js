bc289c3cbd058f7f93431ab944aebc316ba54abc
Vladislav <vlad.satonin74@gmail.com>
2021-12-07 18:37:29 +0500

    KIN-8 add&install prettier & run prettier refactor command

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import type {Node} from 'react';
-import {Text} from 'react-native';
-
-import * as Sentry from '@sentry/react-native';
+import React from 'react'
+import type { Node } from 'react'
+import { Text } from 'react-native'
+import * as Sentry from '@sentry/react-native'
 
 Sentry.init({
   dsn: 'https://19cdb36b586d4e97bb1ae5046ff65564@o1085610.ingest.sentry.io/6096605',
   // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
   // We recommend adjusting this value in production.
-  tracesSampleRate: 1.0,
-});
+  tracesSampleRate: 1.0
+})
 
 const App: () => Node = () => {
-  return <Text>Hello World!!))!</Text>;
-};
+  return <Text>Hello World!!))!</Text>
+}
 
-Sentry.wrap(App);
-export default App;
+Sentry.wrap(App)
+export default App

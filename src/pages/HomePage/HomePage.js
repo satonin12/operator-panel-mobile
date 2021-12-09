db7d1644d93cc6ae27919e8de305c06410b46e93
Vladislav <vlad.satonin74@gmail.com>
2021-12-09 19:21:58 +0500

    KIN-12 after sending form a question redirect to queuePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,30 +1,76 @@
 import React from 'react'
-import {
-  Box
-} from 'native-base'
+import { Box } from 'native-base'
 import { useFormik } from 'formik'
+import { firebase } from '@react-native-firebase/database'
+import uuid from 'react-native-uuid'
+import {Actions} from 'react-native-router-flux'
 
-import { StartForm } from "../../components/forms/StartForm/StartForm";
-import { StartFormSchema } from "../../utils/validation";
+import { StartForm } from '../../components/forms/StartForm/StartForm'
+import { StartFormSchema } from '../../utils/validation'
 
 export const HomePage = () => {
+  const createDialogFromBack = async (values) => {
+    console.log('зашли сюда')
+    let lengthActiveDialogs
+    // создаем запись
+    // для этого узнаем длину последнего элемента в очереди
+    await firebase
+      .database()
+      .ref('chat/start/')
+      // .limitToLast(1)
+      .once('value', (snapshot) => {
+        if (snapshot.val() === 'null' || snapshot.val() === null) {
+          lengthActiveDialogs = 0
+        } else {
+          const lengthDialogs = Object.keys(snapshot.val())
+          lengthActiveDialogs = Number(lengthDialogs[lengthDialogs.length - 1]) + 1
+        }
+      })
+    const timestamp = new Date()
+    const newDialog = {
+      operatorId: 0, // оператор пока что не присвоен => 0
+      status: 'start', // статус диалога - очередь
+      uuid: uuid.v4(),
+      name: values.name,
+      avatar: '',
+      messages: [
+        {
+          content: values.message,
+          writtenBy: 'client',
+          timestamp: timestamp.toISOString()
+        }
+      ]
+    }
+    
+    await firebase
+      .database()
+      .ref(`chat/start/${lengthActiveDialogs}`)
+      .set(newDialog, (error) => {
+        if (error) {
+          console.log(error)
+        } else {
+          console.log('добавление прошло удачно - смотри firebase')
+        }
+      })
+    
+    Actions.queue()
+  }
+
   const formik = useFormik({
     initialValues: {
       name: '',
       topics: '',
-      subtopics: ''
+      subtopics: '',
+      message: ''
     },
     validationSchema: StartFormSchema,
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
+    onSubmit: (values) => {
+      createDialogFromBack(values)
     }
   })
-  
+
   return (
-    <Box
-      width='100%'
-      padding='15px'
-    >
+    <Box width="100%" padding="15px">
       <StartForm formik={formik} />
     </Box>
   )
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
-// import { HomePage } from '../pages/HomePage/HomePage'
+import { HomePage } from '../pages/HomePage/HomePage'
 import { QueuePage } from '../pages/QueuePage/QueuePage'
 
 export const Routes = [
-  // {
-  //   key: 'home',
-  //   title: 'Начальный экран',
-  //   component: HomePage
-  // },
+  {
+    key: 'home',
+    title: 'Начальный экран',
+    component: HomePage
+  },
   {
     key: 'queue',
     title: 'Очередь',
